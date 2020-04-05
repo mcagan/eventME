3262d5d3bdc13073f956e13004f9dfd8761620ba
mcagan <meliscagan@gmail.com>
2020-04-05 15:10:36 +0000

    create initial db schemas

---
+++ db/schema/05_comments.sql
@@ -0,0 +1,10 @@
+-- Drop and recreate Widgets table (Example)
+
+DROP TABLE IF EXISTS comments CASCADE;
+CREATE TABLE comments (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) CASCADE ON DELETE,
+  events_id INTEGER REFERENCES events(id) CASCADE ON DELETE,
+  comment TEXT NOT NULL,
+  posted_on DATE NOT NULL
+);
