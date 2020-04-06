d61864725a935baa35204d7008a9e3c8c2dbd4fd
Luana Duarte <lua.duarte04@gmail.com>
2020-04-06 23:11:30 +0000

    updating event route and adding fetching database on event.ejs file

--- db/schema/02_events.sql
+++ db/schema/02_events.sql
@@ -1,3 +1,4 @@
+-- figure it out how to add cascade after
 DROP TABLE IF EXISTS events CASCADE;
 CREATE TABLE events (
   id SERIAL PRIMARY KEY NOT NULL,
@@ -5,5 +6,5 @@ CREATE TABLE events (
   title VARCHAR(255) NOT NULL,
   description VARCHAR(255),
   location VARCHAR(255),
-  user_id INTEGER REFERENCE users(id) ON DELETE CASCADE
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
--- public/scripts/event.js
+++ public/scripts/event.js
@@ -31,18 +31,10 @@ const createListOfDates = function(listObj) {
         .attr('name', 'to-be-decided')
         .attr('value', 'to-be-decided');
 
-  // this closes all elements appenddint to its parent
-  // $divShareLink.appendTo($divEventHeader);
-  // $pByUser.appendTo($divEventHeader);
-  // $divEventHeader.appendTo($divMainWrapper);
-  // $divParticipantContainer.appendTo(divPollContainer);
   $divDate.appendTo($liListRow);
   $divVoteCounter.appendTo($liListRow);
   $inputChackBox.appendTo($liListRow);
   $liListRow.appendTo($ulcontainAllList);
-  // $divPollList.appendTo($divPollContainer);
-  // $divParticipantContainer.appendTo($divPollContainer);
-  // $divPollContainer.appendTo($divMainWrapper);
 
   // return final tweet element
   return $ulcontainAllList;
@@ -53,59 +45,3 @@ const createListOfDates = function(listObj) {
 $(document).ready(function() {
   createListOfDates();
 });
-
-
-
-
-
-
-// IT WAS ALL DYNAMIC BUT APPERANTELY WE DONT NEED TO BE//////
-/////////////
-// let $divMainWrapper = $('<div>').addClass('main-wrapper');
-
-// // ADDS A HEADER FOR EVENT'S TITLE, USER, DATE AND LOCATION + SHARE BUTTONS
-// let $divEventHeader = $('<div>').addClass('event-header');
-//   $('<h2>').appendTo($divEventHeader);
-//   let $pByUser = $('<p>');
-//     $('<span>')
-//       .addClass('bolder-weigth')
-//       .text('By: ')
-//       .appendTo($pByUser);
-//   let $pByDate = $('<p>');
-//     $('<span>')
-//       .addClass('bolder-weigth')
-//       .text('On: ')
-//       .appendTo($pByDate);
-//   let $pByLocation = $('<p>');
-//     $('<span>')
-//       .addClass('bolder-weigth')
-//       .text('Location: ')
-//       .appendTo($pByLocation);
-//   let $divShareLink = $('<div>').addClass('a2a_kit a2a_kit_size_32 a2a_default_style')
-//     $('<a>')
-//       .addClass('a2a_button_emai')
-//       .appendTo($divShareLink);
-//     $('<a>')
-//       .addClass('a2a_button_facebook_messenger')
-//       .appendTo($divShareLink);
-//     $('<a>')
-//       .addClass('a2a_button_linkedin')
-//       .appendTo($divShareLink);
-//     $('<a>')
-//       .addClass('a2a_button_copy_link')
-//       .appendTo($divShareLink);
-
-
-// // EVENT POLL
-// let $divPollContainer = $('<div>').addClass('poll_container');
-// // user name
-//   let $divParticipantContainer = $('<div>').addClass('poll_container_participan');
-//     $('<input>')
-//       .attr('type', 'text')
-//       .attr('value', 'Luana Duarte')
-//       .attr('id', 'participant_name')
-//       .attr('required', 'required')
-//       .attr('maxlength', '64')
-//       .appendTo($divParticipantContainer);
-//   // poll table
-//   let $divPollList = $('div.poll_container_list');
