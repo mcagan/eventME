b99a8d562031a511ce6e21dda55e86bf49ea7400
Flora <flora.uwadiegwu@gmail.com>
2020-04-06 14:31:34 +0000

    update db seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,3 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (id, name, email, cookie_id) VALUES (1, 'Alice', 'alice88@gmail.com', 65154);
+INSERT INTO users (id, name, email, cookie_id) VALUES (2, 'Kira', 'ki2898@yahoo.com', 76568);
