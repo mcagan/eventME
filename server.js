5b2291ab99944ee629eda202ef5a774f603dfb87
Luana Duarte <lua.duarte04@gmail.com>
2020-04-06 16:43:34 +0000

    updating server routes

--- server.js
+++ server.js
@@ -35,20 +35,17 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
+// routes for all UI - event / index / create event
+const uiRoutes = require("./routes/ui");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
+// routes for all UI - event / index / create event
+app.use("/", uiRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
-
-// Home page
-// Warning: avoid creating more routes in this file!
-// Separate them into separate routes files (see above).
-app.get("/", (req, res) => {
-  res.render("index");
-});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
