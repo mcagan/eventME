9f7521c720751a614dea89793fe26c932d6d20ad
mcagan <meliscagan@gmail.com>
2020-04-07 21:09:35 +0000

    create variable to store user generated events

--- public/scripts/createEvent.js
+++ public/scripts/createEvent.js
@@ -9,25 +9,59 @@ const generateUniqueURL = (num) => {
 };
 module.exports = { generateUniqueURL };
 
-const addUser = function (data) {
-  const queryText =
-    "INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *;";
-  const values = [data.name, data.email];
-  return pool
-    .query(queryText, values)
-    .then((res) => res.rows)
-    .catch((err) => console.log("Err", err));
-};
-exports.addUser = addUser;
+$(document).ready(() => {
+  $("#submit").submit(function (event) {
+    event.preventDefault();
+    let outputObj = {};
+    outputObj.title = event.text[0];
+    outputObj.description = event.text[1];
+    outputObj.location = event.text[2];
+    outputObj.name = event.text[3];
+    outputObj.email = event.text[4];
+    outputObj.events = events;
+    date = outputObj.serialize();
+    $.ajax({ method: "POST", url: "/events", data })
+      .done(function (result) {
+        return result;
+      })
+      .fail(function (error) {
+        // Problem with the request
+        console.log(`Error with the request: ${error.message}`);
+      })
+      .always(function () {
+        // This will always run
+        console.log("request completed");
+      });
+  });
+});
 
-const addEvent = function (data, user) {
-  const queryText =
-    "INSERT INTO events (title, URL, description, location, user_id) VALUES ($1, $2, $3, $4, $5) RETURNING *;";
-  const URL = generateUniqueURL(10);
-  const values = [data.title, URL, data.description, data.location, user.id];
-  return pool
-    .query(queryText, values)
-    .then((res) => res.rows)
-    .catch((err) => console.log("Err", err));
-};
-exports.addEvent = addEvent;
+//extract information from the form
+//extract the schedule from the calendar through the calendar variable
+//create an output object that contains the information
+//ajax request post to /events => send the output object along
+//on the backend side receive this object available through req.body
+
+// const addEvent = function (data, user) {
+//   const queryText =
+//     "INSERT INTO events (title, URL, description, location, user_id) VALUES ($1, $2, $3, $4, $5) RETURNING *;";
+//   const URL = generateUniqueURL(10);
+//   const values = [data.title, URL, data.description, data.location, user.id];
+//   return pool
+//     .query(queryText, values)
+//     .then((res) => res.rows)
+//     .catch((err) => console.log("Err", err));
+// };
+// exports.addEvent = addEvent;
+
+// const addUser = function (data) {
+//   const queryText =
+//     "INSERT INTO users (name, email) VALUES ($1, $2) RETURNING id;";
+//   const values = [data.name, data.email];
+//   return pool
+//     .query(queryText, values)
+//     .then((res) => addEvent(data, Number(res.rows[0]).then((res) => ))
+//     .catch((err) => console.log("Err", err));
+// };
+// exports.addUser = addUser;
+
+// const addDate = function (calendar) {};
--- routes/event.js
+++ routes/event.js
@@ -1,30 +1,41 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
 module.exports = (db) => {
   // EVENT ROUTER TO RENDER EVENT PAGE - TEST - BY LUANA
   router.get("/event/:id", (req, res) => {
-    db.query(`
+    db.query(
+      `
       SELECT *
       FROM events
       JOIN users ON user_id = users.id
       WHERE events.id = $1;
-      `, [req.params.id])
-      .then(data => {
+      `,
+      [req.params.id]
+    )
+      .then((data) => {
         const event = data.rows[0];
         console.log(event);
-        res.render('event', { event });
+        res.render("event", { event });
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
   });
 
   router.get("/createEvent", (req, res) => {
     res.render("createEvent");
-  })
+  });
 
+  router.post("/events", (req, res) => {
+    // const queryText =
+    // "INSERT INTO users (name, email) VALUES ($1, $2) RETURNING id;";
+    // const values = [data.name, data.email];
+    // return pool
+    // .query(queryText, values)
+    // .then((res) => addEvent(data, Number(res.rows[0]).then((res) => ))
+    // .catch((err) => console.log("Err", err));
+    console.log(req.body);
+  });
   return router;
-}
+};
