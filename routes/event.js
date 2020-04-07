99b30a80f61c963cc6c6a29a03954c953c161530
Luana Duarte <lua.duarte04@gmail.com>
2020-04-07 22:59:36 +0000

    fixing route to add query for votes

--- routes/event.js
+++ routes/event.js
@@ -5,17 +5,19 @@ module.exports = (db) => {
   // EVENT ROUTER TO RENDER EVENT PAGE - TEST - BY LUANA
   router.get("/event/:id", (req, res) => {
     const ajax = req.query.ajax;
+    // console.log('check this', req.params);
     db.query(`
       SELECT events.title, users.name, events.location, dates.date, COUNT(votes.id) AS vote_count
       FROM events
-      JOIN users ON user_id = users.id
+      JOIN users ON events.user_id = users.id
       JOIN dates ON dates.event_id = events.id
-      JOIN votes ON votes.event_id = events.id
-      WHERE events.id = $1
-      GROUP BY events.id, dates.id;
+      JOIN votes ON votes.event_id = events.id AND votes.user_id = events.user_id
+      GROUP BY events.id, dates.id, users.name
+      HAVING events.id = $1;
       `, [req.params.id])
       .then(data => {
-        const event = data.rows[0];
+        let event = data.rows[0];
+        event.id = req.params.id;
         console.log(event);
         if (ajax) {
           res.json({ event });
