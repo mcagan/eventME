d7b49c9340488276d095d60516b0beef9f1dd454
mcagan <meliscagan@gmail.com>
2020-04-07 19:04:50 +0000

    create addUser and addEvent functions

--- public/scripts/createEvent.js
+++ public/scripts/createEvent.js
@@ -1,27 +1,33 @@
-// $(() => {
-//   $("#submit").submit(function(event) {
-//   event.preventDefault();
-//   $("#error").empty();
-//   let tweetLength = $("textarea").val().length;
-//   let data = $(this).serialize();
-//   if (tweetLength === 0) {
-//     $("#error").text("⚠️ Please enter a tweet");
-//   } else if (tweetLength > 140) {
-//     $("#error").text("⚠️ Character count can't exceed maximum");
-//   } else {
-//     $.ajax({ method: "POST", url: "/tweets", data })
-//       .done(function(result) {
-//         $("#tweet-text").val("");
-//         loadtweets(result);
-//       })
-//       .fail(function(error) {
-//         // Problem with the request
-//         console.log(`Error with the request: ${error.message}`);
-//       })
-//       .always(function() {
-//         // This will always run
-//         console.log("request completed");
-//       });
-//   }
-// })
-// });
+//Generates random string for uniqueURL
+const generateUniqueURL = (num) => {
+  let result = "";
+  let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < num; i++) {
+    result += chars.charAt(Math.floor(Math.random() * 62));
+  }
+  return result;
+};
+module.exports = { generateUniqueURL };
+
+const addUser = function (data) {
+  const queryText =
+    "INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *;";
+  const values = [data.name, data.email];
+  return pool
+    .query(queryText, values)
+    .then((res) => res.rows)
+    .catch((err) => console.log("Err", err));
+};
+exports.addUser = addUser;
+
+const addEvent = function (data, user) {
+  const queryText =
+    "INSERT INTO events (title, URL, description, location, user_id) VALUES ($1, $2, $3, $4, $5) RETURNING *;";
+  const URL = generateUniqueURL(10);
+  const values = [data.title, URL, data.description, data.location, user.id];
+  return pool
+    .query(queryText, values)
+    .then((res) => res.rows)
+    .catch((err) => console.log("Err", err));
+};
+exports.addEvent = addEvent;
