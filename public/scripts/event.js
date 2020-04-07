3f4c4e8a2038b61b53fa5acd6d8ef6d5d2606fcd
Luana Duarte <lua.duarte04@gmail.com>
2020-04-07 19:03:17 +0000

    trying to add dates dynamically

--- public/scripts/event.js
+++ public/scripts/event.js
@@ -6,24 +6,27 @@
 
 // THIS FUNCTION WILL CREATE THE HTML ELEMENTS FOR A NEW EVENT
 const createListOfDates = function(listObj) {
+  // let momentDay = moment(listObj.date);
+  // momentDay.format('MMM Do YY')
+  let momentDates = moment(listObj.date);
+
   let $ulcontainAllList = $('ul.dates_list');
     let $liListRow = $('<li>').addClass('date_list_row');
       let $divDate = $('<div>').addClass('date_option');
         $('<div>')
-          .text('text') // dummy to be removed
           .addClass('d-month')
+          .text(moment(momentDay))
           .appendTo($divDate);
         $('<div>')
-          .text('text') // dummy to be removed
           .addClass('d-date')
           .appendTo($divDate);
         $('<div>')
-          .text('text') // dummy to be removed
           .addClass('d-day')
           .appendTo($divDate);
       let $divVoteCounter = $('<div>').addClass('vote_counter');
         $('<p>')
-          .text('0 votes') // dummy to be removed
+          .text(event.votes)
+          .text(' votes')
           .appendTo($divVoteCounter);
       let $inputChackBox = $('<input>')
         .attr('type', 'checkbox')
@@ -40,8 +43,49 @@ const createListOfDates = function(listObj) {
   return $ulcontainAllList;
 };
 
+// FUNCTION TO RENDER EVENTS COMMING FROM DATABASE?!
+const renderListOfDates = function(ListOfDates) {
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+  $('.dates_list').empty();
+  $.each(ListOfDates, function(index, listObj) {
+    $('.dates_list').append(createListOfDates(listObj));
+  });
+};
+
+// FUNCTION DEALS WITH THE GET REQUEST OF SAVED EVENTS RENDERING TO TIMELINE
+const requestDates = (url) => {
+  // issue the request with jQuery Ajax
+  $.ajax({
+    method: "GET",
+    url
+  })
+    .done(function(result) {
+      // Success. Getting the result from the request
+      // the return element need to be appended to the existing html
+      // i need to get event.date
+      // use moment to generate the appropriate format
+      // pass the value to my template
+      // $('date_option').append(renderListOfDates(result));
+      renderListOfDates(result);
+    })
+    .fail(function(error) {
+      // Problem with the request
+      console.log(`Error with the request: ${error.message}`);
+    })
+    .always(function() {
+      // This will always run
+      console.log('request completed');
+    });
+};
 
 // WHEN DOCUMENT IS READY DO THE FOLLOWING
 $(document).ready(function() {
-  createListOfDates();
+
+  // Reading the id of the event comming from .main-wrapper that has data-id = event.id in the HTML
+  const eventId = $('.main-wrapper').data('id');
+
+  // ?ajax=true -> means that we want to get json data back from the backend route (otherwise we get full HTML)
+  requestDates(`/event/${eventId}?ajax=true`);
 });
--- routes/event.js
+++ routes/event.js
@@ -4,16 +4,22 @@ const router  = express.Router();
 module.exports = (db) => {
   // EVENT ROUTER TO RENDER EVENT PAGE - TEST - BY LUANA
   router.get("/event/:id", (req, res) => {
+    const ajax = req.query.ajax;
     db.query(`
       SELECT *
       FROM events
       JOIN users ON user_id = users.id
+      JOIN dates ON event_id = events.id
       WHERE events.id = $1;
       `, [req.params.id])
       .then(data => {
         const event = data.rows[0];
         console.log(event);
-        res.render('event', { event });
+        if (ajax) {
+          res.json({ event });
+        } else {
+          res.render('event', { event });
+        }
       })
       .catch(err => {
         res
