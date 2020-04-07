9f4de5cb863e3c588248a9110d0742a8ad84873f
Flora <flora.uwadiegwu@gmail.com>
2020-04-07 22:22:35 +0000

    update event.ejs to pull the corrent event_id and comment out comment section since it's TBD

--- public/scripts/event.js
+++ public/scripts/event.js
@@ -5,34 +5,31 @@
  */
 
 // THIS FUNCTION WILL CREATE THE HTML ELEMENTS FOR A NEW EVENT
-const createListOfDates = function(listObj) {
-  console.log(listObj.votes);
+const createListOfDates = function (listObj) {
+  console.log("DATE", listObj.date);
 
-  let $ulcontainAllList = $('ul.dates_list');
-    let $liListRow = $('<li>').addClass('date_list_row');
-      let $divDate = $('<div>').addClass('date_option');
-        $('<div>')
-          .addClass('d-month')
-          .text(moment(listObj.date).format('MMM'))
-          .appendTo($divDate);
-        $('<div>')
-          .addClass('d-date')
-          .text(moment(listObj.date).format('Do'))
-          .appendTo($divDate);
-        $('<div>')
-          .addClass('d-day')
-          .text(moment(listObj.date).format('ddd'))
-          .appendTo($divDate);
-      let $divVoteCounter = $('<div>').addClass('vote_counter');
-        $('<p>')
-          .text(listObj.votes)
-          .text(' votes')
-          .appendTo($divVoteCounter);
-      let $inputCheckBox = $('<input>')
-        .attr('type', 'checkbox')
-        .attr('id', 'poll_checkbox')
-        .attr('name', 'to-be-decided')
-        .attr('value', 'to-be-decided');
+  let $ulcontainAllList = $("ul.dates_list");
+  let $liListRow = $("<li>").addClass("date_list_row");
+  let $divDate = $("<div>").addClass("date_option");
+  $("<div>")
+    .addClass("d-month")
+    .text(moment(listObj.date).format("MMM"))
+    .appendTo($divDate);
+  $("<div>")
+    .addClass("d-date")
+    .text(moment(listObj.date).format("Do"))
+    .appendTo($divDate);
+  $("<div>")
+    .addClass("d-day")
+    .text(moment(listObj.date).format("ddd"))
+    .appendTo($divDate);
+  let $divVoteCounter = $("<div>").addClass("vote_counter");
+  $("<p>").text(listObj.votes).text(" votes").appendTo($divVoteCounter);
+  let $inputCheckBox = $("<input>")
+    .attr("type", "checkbox")
+    .attr("id", "poll_checkbox")
+    .attr("name", "to-be-decided")
+    .attr("value", "to-be-decided");
 
   $divDate.appendTo($liListRow);
   $divVoteCounter.appendTo($liListRow);
@@ -44,13 +41,15 @@ const createListOfDates = function(listObj) {
 };
 
 // FUNCTION TO RENDER EVENTS COMMING FROM DATABASE?!
-const renderListOfDates = function(ListOfDates) {
+const renderListOfDates = function (ListOfDates) {
   // loops through dates
   // calls createListOfDates for each date list
   // takes return value and appends it to the list < ul >
-  $('.dates_list').empty();
-  $.each(ListOfDates, function(index, listObj) {
-    $('.dates_list').append(createListOfDates(listObj));
+  $(".dates_list").empty();
+
+  $.each(ListOfDates, function (index, listObj) {
+    console.log("listObj", listObj);
+    $(".dates_list").append(createListOfDates(listObj));
   });
 };
 
@@ -59,20 +58,20 @@ const requestDates = (url) => {
   // issue the request with jQuery Ajax
   $.ajax({
     method: "GET",
-    url
+    url,
   })
-    .done(function(result) {
+    .done(function (result) {
       // Success. Getting the result from the request
       // the return element need to be appended to the existing html
       renderListOfDates(result);
     })
-    .fail(function(error) {
+    .fail(function (error) {
       // Problem with the request
       console.log(`Error with the request: ${error.message}`);
     })
-    .always(function() {
+    .always(function () {
       // This will always run
-      console.log('request completed');
+      console.log("request completed");
     });
 };
 
@@ -81,10 +80,9 @@ const requestDates = (url) => {
 ////////////////////////////////////////////////////////////////////
 
 // WHEN DOCUMENT IS READY DO THE FOLLOWING
-$(document).ready(function() {
-
+$(document).ready(function () {
   // Reading the id of the event comming from .main-wrapper that has data-id = event.id in the HTML
-  const eventId = $('.main-wrapper').data('id');
+  const eventId = $(".main-wrapper").data("id");
 
   // ?ajax=true -> means that we want to get json data back from the backend route (otherwise we get full HTML)
   requestDates(`/event/${eventId}?ajax=true`);
--- routes/event.js
+++ routes/event.js
@@ -1,36 +1,38 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
 module.exports = (db) => {
   // EVENT ROUTER TO RENDER EVENT PAGE - TEST - BY LUANA
   router.get("/event/:id", (req, res) => {
     const ajax = req.query.ajax;
-    db.query(`
+    console.log("REQ>QUERY>", req.query);
+    db.query(
+      `
       SELECT *
       FROM events
       JOIN users ON user_id = users.id
       JOIN dates ON event_id = events.id
       WHERE events.id = $1;
-      `, [req.params.id])
-      .then(data => {
+      `,
+      [req.params.id]
+    )
+      .then((data) => {
         const event = data.rows[0];
         console.log(event);
         if (ajax) {
           res.json({ event });
         } else {
-          res.render('event', { event });
+          res.render("event", { event });
         }
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
   });
 
   router.get("/createEvent", (req, res) => {
     res.render("createEvent");
-  })
+  });
 
   return router;
-}
+};
