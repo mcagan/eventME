f73627bd84fd994ec5136ca18552df1e51e656f4
Flora <flora.uwadiegwu@gmail.com>
2020-04-09 18:32:28 +0000

    update jquery to dynamically show data from db in poll

--- public/scripts/event.js
+++ public/scripts/event.js
@@ -3,91 +3,102 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
-// FUNCTION WILL CREATE THE DATES IN HTML FOR AN EVENT
-const createListOfDates = function (listObj) {
-  let $ulcontainAllList = $("ul.dates_list");
-  let $liListRow = $("<li>").addClass("date_list_row");
-  let $divDate = $("<div>").addClass("date_option");
-  $("<div>")
-    .addClass("d-month")
-    .text(moment(listObj.start_date).format("MMM"))
-    .appendTo($divDate);
-  $("<div>")
-    .addClass("d-date")
-    .text(moment(listObj.start_date).format("Do"))
-    .appendTo($divDate);
-  $("<div>")
-    .addClass("d-day")
-    .text(moment(listObj.start_date).format("ddd"))
-    .appendTo($divDate);
-  $("<div>")
-    .addClass("d-time")
-    .text(moment(listObj.start_date).format("h:mm a"))
-    .appendTo($divDate);
-  $("<div>")
-    .addClass("d-time")
-    .text(` - ${moment(listObj.end_date).format("h:mm a")}`)
-    .appendTo($divDate);
-  let $divVoteCounter = $("<div>").addClass("vote_counter");
-  $("<p>").text(`${listObj.vote_count} votes`).appendTo($divVoteCounter);
-  let $inputCheckBox = $("<input>")
-    .attr("type", "checkbox")
-    .attr("id", "poll_checkbox")
-    .attr("name", "to-be-decided")
-    .attr("value", "to-be-decided");
-
-  $divDate.appendTo($liListRow);
-  $divVoteCounter.appendTo($liListRow);
-  $inputCheckBox.appendTo($liListRow);
-  $liListRow.appendTo($ulcontainAllList);
-
-  // return final tweet element
-  return $ulcontainAllList;
-};
-
-// FUNCTION TO RENDER EVENTS COMMING FROM DATABASE?!
-const renderListOfDates = function (ListOfDates) {
-  // loops through dates
-  // calls createListOfDates for each date list
-  // takes return value and appends it to the list < ul >
-  $(".dates_list").empty();
-
-  $.each(ListOfDates, function (index, listObj) {
-    listObj.forEach((item) => {
-      $(".dates_list").append(createListOfDates(item));
-    });
-  });
-};
-
-// FUNCTION DEALS WITH THE GET REQUEST OF SAVED EVENTS RENDERING TO TIMELINE
-const requestDates = (url) => {
-  // issue the request with jQuery Ajax
-  $.ajax({
-    method: "GET",
-    url,
-  })
-    .done(function (result) {
-      // Success. Getting the result from the request
-      // the return element need to be appended to the existing html
-      renderListOfDates(result);
-    })
-    .fail(function (error) {
-      // Problem with the request
-      console.log(`Error with the request: ${error.message}`);
-    })
-    .always(function () {
-      // This will always run
-      console.log("request completed");
-    });
-};
-
-//////////////////////////////////////////////////////////////////////
-/// DO I NEED TO CREATE A POST REQUEST FOR THE VOTES? I THINK YES ///
-////////////////////////////////////////////////////////////////////
-
 // WHEN DOCUMENT IS READY DO THE FOLLOWING
 $(document).ready(function () {
+  // FUNCTION WILL CREATE THE DATES IN HTML FOR AN EVENT
+  const createListOfDates = function (item) {
+    let $ulcontainAllList = $("ul.dates_list");
+    let $liListRow = $("<li>")
+      .addClass("date_list_row")
+      .attr("data-timeslotid", item.date_id);
+    let $divDate = $("<div>").addClass("date_option");
+    $("<div>")
+      .addClass("d-month")
+      .text(moment(item.start_date).format("MMM"))
+      .appendTo($divDate);
+    $("<div>")
+      .addClass("d-date")
+      .text(moment(item.start_date).format("Do"))
+      .appendTo($divDate);
+    $("<div>")
+      .addClass("d-day")
+      .text(moment(item.start_date).format("ddd"))
+      .appendTo($divDate);
+    $("<div>")
+      .addClass("d-time")
+      .text(moment(item.start_date).format("h:mm a"))
+      .appendTo($divDate);
+    $("<div>")
+      .addClass("d-time")
+      .text(` - ${moment(item.end_date).format("h:mm a")}`)
+      .appendTo($divDate);
+    let $divVoteCounter = $("<div>").addClass("vote_counter");
+    $("<p>").text(`${item.vote_count} vote(s)`).appendTo($divVoteCounter);
+
+    // To append vote names on top of vote count
+    let $divNames = $("<div>").addClass("popuptext");
+    for (let user of item.users) {
+      $("<p>").text(user.name).appendTo($divNames);
+    }
+
+    $divNames.prependTo($divVoteCounter);
+
+    let $inputCheckBox = $("<input>")
+      .attr("type", "checkbox")
+      .attr("id", "poll_checkbox")
+      .attr("name", "to-be-decided")
+      .attr("value", "to-be-decided");
+
+    $divDate.appendTo($liListRow);
+    $divVoteCounter.appendTo($liListRow);
+    $inputCheckBox.appendTo($liListRow);
+    $liListRow.appendTo($ulcontainAllList);
+
+    // return final poll elements
+    return $ulcontainAllList;
+  };
+
+  // FUNCTION TO RENDER POLL DATES OF EVENTS COMING FROM DB
+  const renderListOfDates = function (ListOfDates) {
+    // loops through dates
+    // calls createListOfDates for each date list
+    // takes return value and appends it to the list < ul >
+    $(".dates_list").empty();
+
+    $.each(ListOfDates.event, function (index, listObj) {
+      $.each(listObj.timeslots, function (index, item) {
+        $(".dates_list").append(createListOfDates(item));
+      });
+    });
+  };
+
+  // FUNCTION DEALS WITH THE GET REQUEST OF SAVED EVENTS RENDERING TO TIMELINE
+  const requestDates = (url) => {
+    // issue the request with jQuery Ajax
+    $.ajax({
+      method: "GET",
+      url,
+    })
+      .done(function (result) {
+        // Success. Getting the result from the request
+        // the return element need to be appended to the existing html
+        console.log(result);
+        renderListOfDates(result);
+      })
+      .fail(function (error) {
+        // Problem with the request
+        console.log(`Error with the request: ${error.message}`);
+      })
+      .always(function () {
+        // This will always run
+        console.log("request completed");
+      });
+  };
+
+  //////////////////////////////////////////////////////////////////////
+  /// DO I NEED TO CREATE A POST REQUEST FOR THE VOTES? I THINK YES ///
+  ////////////////////////////////////////////////////////////////////
+
   // Reading the id of the event comming from .main-wrapper that has data-id = event.id in the HTML
   const eventId = $(".main-wrapper").data("id");
 
@@ -95,4 +106,23 @@ $(document).ready(function () {
 
   // ?ajax=true -> means that we want to get json data back from the backend route (otherwise we get full HTML)
   requestDates(`/event/${eventId}?ajax=true`);
+
+  //////////////////////////////////////////
+  ////////// EVENT HANDLERS ///////////////
+
+  //To show/hide voter names on click
+  $(".dates_list").on("click", ".vote_counter", function (event) {
+    let namesNotDisplayed = $(".popuptext").hasClass("popup-show");
+    if (!namesNotDisplayed) {
+      $(this).find(".popuptext").addClass("popup-show");
+    } else {
+      $(this).find(".popuptext").removeClass("popup-show");
+    }
+  });
 });
+
+//Handler on submit (prevent default)
+//then extract info from the form and put them in an object
+//Issue an ajax POST request action "/votes" (check with Luana)
+//update the UI (select query)
+//create the dynamic element/template for the user and append to html
--- routes/test.js
+++ routes/test.js
@@ -1,54 +1,102 @@
 {
-  event_id: 1,
-  title: 'Meetup with Chicago Stakeholders',
-  admin: {id: 3, name: 'Kira', email: 'email@gmail.com'},
-  location: 'Montreal',
-  timeslots: [
+  event = [
     {
-
-      timeslotId: 1,
-
-      start_date: 2019-02-12T15:00:00.000Z,
-      end_date: 2019-02-12T16:00:00.000Z },
-      users: [{id: 1, username: 'Bob', votes: 2},{id: 2, username: 'Luigi', votes: 3}]
-
+      event_id: 1,
+      title: "Meetup with Chicago Stakeholders",
+      location: "Montreal",
+      user_id: 2,
+      name: "Kira",
+      email: "ki2898@yahoo.com",
+      timeslots: [
+        {
+          date_id: 1,
+          start_date: "2019-02-10T13:00:00.000Z",
+          end_date: "2019-02-10T14:00:00.000Z",
+          users: [
+            {
+              user_id: 1,
+              name: "Alice",
+              email: "alice88@gmail.com",
+              vote_id: 1,
+              yes_no: true,
+            },
+            {
+              user_id: 2,
+              name: "Kira",
+              email: "ki2898@yahoo.com",
+              vote_id: 2,
+              yes_no: true,
+            },
+          ],
+          vote_count: 2,
+        },
+        {
+          date_id: 2,
+          start_date: "2019-02-11T14:00:00.000Z",
+          end_date: "2019-02-11T15:00:00.000Z",
+          users: [
+            {
+              user_id: 1,
+              name: "Alice",
+              email: "alice88@gmail.com",
+              vote_id: 3,
+              yes_no: true,
+            },
+            {
+              user_id: 2,
+              name: "Kira",
+              email: "ki2898@yahoo.com",
+              vote_id: 4,
+              yes_no: true,
+            },
+          ],
+          vote_count: 2,
+        },
+        {
+          date_id: 3,
+          start_date: "2019-02-12T15:00:00.000Z",
+          end_date: "2019-02-12T16:00:00.000Z",
+          users: [
+            {
+              user_id: 1,
+              name: "Alice",
+              email: "alice88@gmail.com",
+              vote_id: 5,
+              yes_no: true,
+            },
+            {
+              user_id: 2,
+              name: "Kira",
+              email: "ki2898@yahoo.com",
+              vote_id: 6,
+              yes_no: true,
+            },
+          ],
+          vote_count: 2,
+        },
+        {
+          date_id: 4,
+          start_date: "2019-02-13T16:00:00.000Z",
+          end_date: "2019-02-13T17:00:00.000Z",
+          users: [
+            {
+              user_id: 1,
+              name: "Alice",
+              email: "alice88@gmail.com",
+              vote_id: 7,
+              yes_no: true,
+            },
+            {
+              user_id: 2,
+              name: "Kira",
+              email: "ki2898@yahoo.com",
+              vote_id: 8,
+              yes_no: true,
+            },
+          ],
+          vote_count: 2,
+        },
+      ],
     },
-    {
-      timeslotId: 2,
-      start_date: 2019-02-13T16:00:00.000Z,
-      end_date: 2019-02-13T17:00:00.000Z },
-      users: []
-    }
-  ]
+  ];
 }
-
-
-
-
-
-[ anonymous {
-  title: 'Meetup with Chicago Stakeholders',
-  name: 'Kira',
-  location: 'Montreal',
-  start_date: 2019-02-12T15:00:00.000Z,
-  end_date: 2019-02-12T16:00:00.000Z },
-  users: [{id: 1, username: 'Bob', votes: 2},{id: 2, username: 'Luigi', votes: 3}]
-anonymous {
-  title: 'Meetup with Chicago Stakeholders',
-  name: 'Kira',
-  location: 'Montreal',
-  start_date: 2019-02-13T16:00:00.000Z,
-  end_date: 2019-02-13T17:00:00.000Z },
-anonymous {
-  title: 'Meetup with Chicago Stakeholders',
-  name: 'Kira',
-  location: 'Montreal',
-  start_date: 2019-02-11T14:00:00.000Z,
-  end_date: 2019-02-11T15:00:00.000Z },
-anonymous {
-  title: 'Meetup with Chicago Stakeholders',
-  name: 'Kira',
-  location: 'Montreal',
-  start_date: 2019-02-10T13:00:00.000Z,
-  end_date: 2019-02-10T14:00:00.000Z },
-id: '1' ]
